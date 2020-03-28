fb23ca70073454963ea7c51397506a29daba0b41
Anthony DiPietrantonio <antdp425@aol.com>
2020-03-28 11:24:20 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,22 @@
+class Dog
+# def initialize(string)
+#   @name = string
+# end
+
+def name=(string)
+  @name = string
+end
+
+def name
+  @name
+end
+
+def job=(job)
+  @job = job
+end
+
+def job
+  @job
+end
+
+end
